15f4906fc1cee75be44862dc1c6906ac8b24d2f0
MultiElio <eliov@hotmail.fr>
2019-08-22 12:17:03 +0200

    init

---
+++ src/fr/polielio/mc/Takedamage.java
@@ -0,0 +1,33 @@
+package fr.polielio.mc;
+
+import org.bukkit.entity.Creeper;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+
+public class Takedamage implements Listener {
+
+    @EventHandler
+    public void taked(EntityDamageByEntityEvent e) {
+        if(!(e.getEntity() instanceof Player))return;
+        if((!(e.getDamager() instanceof Creeper)) && (!( e.getCause() ==EntityDamageEvent.DamageCause.ENTITY_EXPLOSION)))return;
+        e.setCancelled(true);
+        Player p = (Player) e.getEntity();
+        p.damage(40);
+        p.sendMessage("§c You have been instant kill by a creeper !");
+    }
+
+    @EventHandler
+    public void taked(EntitySpawnEvent e) {
+        if(!(e.getEntity() instanceof Creeper))return;
+        ((Creeper) e.getEntity()).addPotionEffect(new PotionEffect(PotionEffectType.SPEED,600000,2));
+    }
+}
+
+
---
+++ src/fr/polielio/mc/mc.java
@@ -0,0 +1,17 @@
+package fr.polielio.mc;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class mc extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        PluginManager pm = Bukkit.getServer().getPluginManager();
+        pm.registerEvents(new Takedamage(), this);
+    }
+    @Override
+    public void onDisable(){
+
+    }
+}
